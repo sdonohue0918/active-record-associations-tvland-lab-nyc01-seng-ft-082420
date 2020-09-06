2a9922a1b29fcda921046c57e56c41f5c9baf14c
sdonohue0918 <sdonohue0918@gmail.com>
2020-09-06 13:58:29 -0400

    5 tests didn't pass. Will update.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,19 @@
 class Actor < ActiveRecord::Base
   
+  has_many :characters
+  has_many :shows, through: :characters
+
+
+    def list_full_name
+        self.first_name + " " + self.last_name
+  
+    end
+
+    def list_shows_characters
+        quick_imdb = [] 
+        quick imdb << characters.select{|character| character.name}
+        quick imdb << characters.select{|character| character.show.name}
+        quick imdb.flatten
+        quick imdb
+    end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+    def actors_list
+        characters.select {|character| character.actor.list_full_name}
+        #actor.all.map {|actor| actor.list_full_name}
+
   
+
+
+    end
 end
